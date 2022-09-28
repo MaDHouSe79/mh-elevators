52054cc2ebc0d9746b50199b8a0b5e328a22531c
MaDHouSe79 <madhouse1979@gmail.com>
2022-09-28 14:41:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,9 +7,13 @@ Config = {}
 Config.UseTableSort = false
 Config.ShowBlips = true
 
+Config.UseTarget = false 
+
 Config.Elevators = {
 
     ['pillbox_hospital_elevator'] = {
+
+        ['authorized'] = {"police", "ambulance"}, -- job or gang or public
 
         ['blip'] = {
             ['label'] = "Pillbox Hospital Elevator",
@@ -22,15 +26,14 @@ Config.Elevators = {
 
         ['floors'] = {
             [0] = { -- basement
-                label = "",
                 coords = vector3(341.05, -580.84, 28.8), 
                 heading = 86.6,
+                minz = 1.0,
                 tpVehicle = false,
             },
 
             [1] = { -- ground floor
-                label = "",
-                coords = vector3(332.17, -595.61, 43.28),
+                coords = vector3(332.0970, -595.5458, 43.2841),
                 heading = 68.44,
                 tpVehicle = false,
             },
@@ -45,8 +48,10 @@ Config.Elevators = {
 
     ['red_garage_elevator'] = {
 
+        ['authorized'] = {"public"}, -- job or gang or public
+
         ['blip'] = {
-            ['label'] = "Red Garage Elevator",
+            ['label'] = "Red Garage Lift",
             ['show'] = false,
             ['coords'] = vector3(-322.65, -774.91, 33.96),
             ['sprite'] = 728,
@@ -55,25 +60,25 @@ Config.Elevators = {
         },
 
         ['floors'] = {
-            [1] = { -- ground floor
+            [0] = { -- ground floor
                 coords = vector3(-322.65, -774.91, 33.96),
                 heading = 48.92,
                 tpVehicle = false,
             },
 
-            [2] = { 
+            [1] = { 
                 coords = vector3(-322.5, -774.88, 38.78),
                 heading = 48.83,
                 tpVehicle = false,
             },
 
-            [3] = {
+            [2] = {
                 coords = vector3(-322.43, -774.9, 43.61),
                 heading = 47.51,
                 tpVehicle = false,
             },
 
-            [4] = { -- roof
+            [3] = { -- roof
                 coords = vector3(-322.37, -774.92, 53.25),
                 heading = 41.19,
                 tpVehicle = false,
@@ -81,11 +86,13 @@ Config.Elevators = {
         }
 
     },
- 
+
     ['maze_bank_vehicle_elevator'] = {
 
+        ['authorized'] = {"public"}, -- job or gang or public
+
         ['blip'] = {
-            ['label'] = "Maze Bank Vehicle Elevator",
+            ['label'] = "Maze Bank Vehicle Lift",
             ['show'] = false,
             ['coords'] = vector3(-84.03, -820.96, 35.62),
             ['sprite'] = 728,
@@ -108,10 +115,12 @@ Config.Elevators = {
         }
     },
 
-    ['police_elevator']= {
+    ['politieburo_elevator'] = {
+
+        ['authorized'] = {"police", "ambulance"}, -- job or gang or public
 
         ['blip'] = {
-            ['label'] = "Politie List",
+            ['label'] = "Politie Lift",
             ['show'] = false,
             ['coords'] = vector3(-1097.7673, -848.4358, 4.8841),
             ['sprite'] = 728,
@@ -169,4 +178,38 @@ Config.Elevators = {
             },
         }
     },
+
+    ['diamond_casino'] = {
+
+        ['authorized'] = {"public"}, -- job or gang or public
+
+        ['blip'] = {
+            ['label'] = "Diamond Casino Lift",
+            ['show'] = false,
+            ['coords'] = vector3(960.5756, 43.4318, 71.7007),
+            ['sprite'] = 728,
+            ['colour'] = 44,
+            ['scale'] = 0.8,
+        },
+
+        ['floors'] = {
+            [0] = { -- basement
+                coords = vector3(960.5756, 43.4318, 71.7007), 
+                heading = 284.6085,
+                tpVehicle = false,
+            },
+
+            [1] = { -- ground floor
+                coords = vector3(964.9559, 58.6617, 112.5531),
+                heading = 80.0243,
+                tpVehicle = false,
+            },
+            [2] = { -- ground floor
+                coords = vector3(971.8812, 51.9888, 120.2407),
+                heading = 327.7185,
+                tpVehicle = false,
+            },
+        }
+    },
+
 }
