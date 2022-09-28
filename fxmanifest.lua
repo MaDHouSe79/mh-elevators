1cb1455ebb9074a8725ff2605aefc2a418e56553
MaDHouSe79 <madhouse1979@gmail.com>
2022-09-28 14:41:23 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -16,6 +16,11 @@ shared_scripts {
 }
 
 client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua',
 }
 
